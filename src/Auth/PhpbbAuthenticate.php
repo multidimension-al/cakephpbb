54c2f2604190914d2ff44cd352e140ac3347d13d
AJ Quick <ajquick@gmail.com>
2019-01-19 20:37:37 -0700

    Create PhpbbAuthenticate.php
    Basic structure of an authentication app.

---
+++ src/Auth/PhpbbAuthenticate.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *      __  ___      ____  _     ___                           _                    __
+ *     /  |/  /_  __/ / /_(_)___/ (_)___ ___  ___  ____  _____(_)___  ____   ____ _/ /
+ *    / /|_/ / / / / / __/ / __  / / __ `__ \/ _ \/ __ \/ ___/ / __ \/ __ \ / __ `/ /
+ *   / /  / / /_/ / / /_/ / /_/ / / / / / / /  __/ / / (__  ) / /_/ / / / // /_/ / /
+ *  /_/  /_/\__,_/_/\__/_/\__,_/_/_/ /_/ /_/\___/_/ /_/____/_/\____/_/ /_(_)__,_/_/
+ *
+ * CakePHP Plugin : CakePHP Authentication Plugin for phpBB
+ * Copyright (c) Multidimension.al (http://multidimension.al)
+ * Github : https://github.com/multidimension-al/cakephpbb
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE file
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright  Copyright © 2019 Multidimension.al (http://multidimension.al)
+ * @link       https://github.com/multidimension-al/cakephpbb Github
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+ 
+namespace Multidimensional\Cakephpbb\Auth;
+
+use Cake\Auth\BaseAuthenticate;
+use Cake\Controller\ComponentRegistry;
+use Cake\Core\Configure;
+use Cake\Http\Exception\UnauthorizedException;
+use Cake\Http\Response;
+use Cake\Http\ServerRequest;
+
+class PhpbbAuthenticate extends BaseAuthenticate
+{
+
+}
